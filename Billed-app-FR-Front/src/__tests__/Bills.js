054545b42244cedb9bb0af4cdb3037e5270811ef
squal94 <lounis.khadir@wanadoo.fr>
2022-06-30 20:46:10 +0200

    deuxieme test bill container

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -10,6 +10,10 @@ import { localStorageMock } from "../__mocks__/localStorage.js";
 
 import router from "../app/Router.js";
 
+//Nouvelles Imports pour la suites des tests
+
+import Bills from "../containers/Bills.js";
+
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
@@ -48,40 +52,26 @@ describe("Given I am connected as an employee", () => {
   });
 });
 
+// test de click handleClickNewBill
+
 describe("Given I reate a new bill", () => {
   describe("When I click on buttonNewBill", () => {
     test("Then launch function handleClickNewBill", async () => {
-      Object.defineProperty(window, "localStorage", {
-        value: localStorageMock,
+      const btnBills = document.createElement("button");
+      btnBills.setAttribute("data-testid", "btn-new-bill");
+      document.body.append(btnBills);
+
+      btnBills.click;
+
+      new Bills({
+        document,
+        onNavigate,
+        store: null,
+        localStorage,
       });
-      window.localStorage.setItem(
-        "user",
-        JSON.stringify({
-          type: "Employee",
-        })
-      );
-      const root = document.createElement("div");
-      root.setAttribute("id", "root");
-      document.body.append(root);
-      router();
-      window.onNavigate(ROUTES_PATH.Bills);
-      await waitFor(() => screen.getByTestId("icon-window"));
-      //expect n'est pas fourni et la constante windowIcon n'est pas utilisé
-      const windowIcon = screen.getByTestId("icon-window");
-      //to-do write expect expression
-      expect(windowIcon).toBeTruthy();
-      //expect(windowIcon.id).toBeEqual("icon-window");
-    });
-    test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills });
-      const dates = screen
-        .getAllByText(
-          /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
-        )
-        .map((a) => a.innerHTML);
-      const antiChrono = (a, b) => (a < b ? 1 : -1);
-      const datesSorted = [...dates].sort(antiChrono);
-      expect(dates).toEqual(datesSorted);
+
+      expect(handleClickNewBill).toHaveBeenCalled();
+      //expect(handleClickIconEye()).toHaveBeenCalled();
     });
   });
 });
