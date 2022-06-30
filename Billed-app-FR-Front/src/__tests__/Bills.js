97a2189d683e62f8adbc090bcb4de589eff671e7
squal94 <lounis.khadir@wanadoo.fr>
2022-06-30 17:05:41 +0200

    premier test bill container

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -47,3 +47,41 @@ describe("Given I am connected as an employee", () => {
     });
   });
 });
+
+describe("Given I reate a new bill", () => {
+  describe("When I click on buttonNewBill", () => {
+    test("Then launch function handleClickNewBill", async () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+      await waitFor(() => screen.getByTestId("icon-window"));
+      //expect n'est pas fourni et la constante windowIcon n'est pas utilisé
+      const windowIcon = screen.getByTestId("icon-window");
+      //to-do write expect expression
+      expect(windowIcon).toBeTruthy();
+      //expect(windowIcon.id).toBeEqual("icon-window");
+    });
+    test("Then bills should be ordered from earliest to latest", () => {
+      document.body.innerHTML = BillsUI({ data: bills });
+      const dates = screen
+        .getAllByText(
+          /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
+        )
+        .map((a) => a.innerHTML);
+      const antiChrono = (a, b) => (a < b ? 1 : -1);
+      const datesSorted = [...dates].sort(antiChrono);
+      expect(dates).toEqual(datesSorted);
+    });
+  });
+});
