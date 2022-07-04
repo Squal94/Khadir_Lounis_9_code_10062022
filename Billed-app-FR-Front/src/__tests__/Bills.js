df79c13122c5ab7313be3e94c9ad5ea2cd657967
squal94 <lounis.khadir@wanadoo.fr>
2022-07-04 19:32:55 +0200

    test integration test a partir de exemple de dashboard

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -14,7 +14,7 @@ import router from "../app/Router.js";
 //Nouvelles Imports pour la suites des tests
 
 import Bills from "../containers/Bills.js";
-import { modal } from "../views/DashboardFormUI.js";
+//import { modal } from "../views/DashboardFormUI.js";
 //import modal from "../views/BillsUI.js";
 const onNavigate = (pathname) => {
   document.body.innerHTML = ROUTES({ pathname });
@@ -120,6 +120,75 @@ describe("Given I want to see an bill", () => {
         });
 
       expect(screen.getByText("Justificatif")).toBeTruthy;
+    });
+  });
+});
+
+describe("Given I am a user connected as Admin", () => {
+  describe("When I navigate to Dashboard", () => {
+    test("fetches bills from mock API GET", async () => {
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ type: "Admin", email: "a@a" })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Dashboard);
+      await waitFor(() => screen.getByText("Validations"));
+      const contentPending = await screen.getByText("En attente (1)");
+      expect(contentPending).toBeTruthy();
+      const contentRefused = await screen.getByText("Refusé (2)");
+      expect(contentRefused).toBeTruthy();
+      expect(screen.getByTestId("big-billed-icon")).toBeTruthy();
+    });
+    describe("When an error occurs on API", () => {
+      beforeEach(() => {
+        jest.spyOn(mockStore, "bills");
+        Object.defineProperty(window, "localStorage", {
+          value: localStorageMock,
+        });
+        window.localStorage.setItem(
+          "user",
+          JSON.stringify({
+            type: "Admin",
+            email: "a@a",
+          })
+        );
+        const root = document.createElement("div");
+        root.setAttribute("id", "root");
+        document.body.appendChild(root);
+        router();
+      });
+      test("fetches bills from an API and fails with 404 message error", async () => {
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => {
+              return Promise.reject(new Error("Erreur 404"));
+            },
+          };
+        });
+        window.onNavigate(ROUTES_PATH.Dashboard);
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 404/);
+        expect(message).toBeTruthy();
+      });
+
+      test("fetches messages from an API and fails with 500 message error", async () => {
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => {
+              return Promise.reject(new Error("Erreur 500"));
+            },
+          };
+        });
+
+        window.onNavigate(ROUTES_PATH.Dashboard);
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 500/);
+        expect(message).toBeTruthy();
+      });
     });
   });
 });
